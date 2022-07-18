ab9e55920e3990fe22db17ddc397e43bfc74780f
Marina <linamamarinina@gmail.com>
2022-07-18 17:22:24 +0300

    Вывод и упорядочивание массива

--- 3_CSharp_Lecture/Program.cs
+++ 3_CSharp_Lecture/Program.cs
@@ -114,4 +114,72 @@ for(int i = 2; i <= 10; i++)
     Console.WriteLine(); //Для пустой строки после результатов по каждой цифре.
 }
 */
- 
+/*
+//====================
+ // Дан текст. В тексте нужно все пробелы заменить черточками, маленькие буквы "к" заменить большими "К", 
+ // а большие "C" заменить маленькими "c".
+ //19.30
+
+ string text = "- Я думаю, - сказал князь, улыбаясь, - что, ежели бы вас послали вместо нашего милого "
+             + "Винценгероде, вы бы взяли приступом согласие прусского короля."
+             + "Вы так красноречивы. Вы дадите мне чаю?";
+// string s = "qwerty"
+//             012
+//s[3] // r
+
+string Replace(string text, char oldValue, char newValue)
+{
+    string result = String.Empty;
+
+    int length = text.Length;
+    for(int i = 0; i < length; i++)
+    {
+        if(text[i] == oldValue) result = result + $"{newValue}";
+        else result = result + $"{text[i]}";
+    }
+    return result;
+}
+string newText = Replace(text, ' ', '|');
+
+Console.WriteLine(newText);
+Console.WriteLine();
+newText = Replace(newText, 'к', 'К');
+Console.WriteLine(newText);
+newText = Replace(newText, 'С', 'с');
+Console.WriteLine(newText);
+*/
+//====================
+// Упорядочивание данных внутри массива.
+int[] arr ={1,5,4,3,2,6,7,1,1};
+//Подпрограмма для вывода массива на экран.
+// 1 вариант
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for(int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+//Метод, кот.будет упорядочивать массив.
+void SelectionSort(int[] array)
+{
+    for(int i = 0; i < array.Length - 1; i++)
+    {
+        int minPosition = i;
+        
+        for (int j = i + 1; j < array.Length; j++) //ищем нужный элемент.
+        {
+            if(array[j] < array[minPosition]) minPosition = j;
+        }
+        int temporary = array[i];  // переносим.
+        array[i] = array[minPosition];
+        array[minPosition] = temporary;
+    }
+}
+    
+PrintArray(arr);
+SelectionSort(arr);
+
+PrintArray(arr);
