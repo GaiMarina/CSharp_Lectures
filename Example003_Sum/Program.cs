8c697c6bb0102b980f8b0d2a29c67672a1aac81b
Marina <linamamarinina@gmail.com>
2022-07-19 16:50:29 +0300

    Треугольник Серпинского

--- Example003_Sum/Program.cs
+++ Example003_Sum/Program.cs
@@ -1,6 +1,6 @@
 ﻿int numberA = new Random().Next(1, 10); //1 2 3 4 ... 9
-Console.WriteLine(numberA);
+Console.Write(numberA + " ");
 int numberB = new Random().Next(1, 10);
-Console.WriteLine(numberB);
+Console.Write(numberB + " ");
 int result = numberA + numberB;
-Console.WriteLine(result);
+Console.WriteLine(result + " ");
