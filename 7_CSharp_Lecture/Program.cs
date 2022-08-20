10ed8771a011c08f58f12e543ada0cd76754994d
Marina <linamamarinina@gmail.com>
2022-08-20 11:58:30 +0300

    Рекурсия от 1 до n

--- 7_CSharp_Lecture/Program.cs
+++ 7_CSharp_Lecture/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿/*
 // 1. Собрать строку с числами от a до b, a <= b.
 
 string NumbersFor(int a, int b)
@@ -20,4 +20,48 @@ string NumbersRec(int a, int b)
 }
 
 Console.WriteLine(NumbersFor(1,10)); //10 входит.
-Console.WriteLine(NumbersRec(1,10));
+Console.WriteLine(NumbersRec(1,10)); //10 входит.
+*/
+//=========================
+
+// 1 задача в обратном порядке.
+// 1. Собрать строку с числами от a до b, a >= b.
+
+string NumbersFor(int a, int b)
+{
+    string result = string.Empty;
+    for(int i = b; i >= a; i--)
+    {
+        result += $"{i} ";
+    }
+    return result;
+}
+
+string NumbersRec(int a, int b)
+{
+    if(a <= b) return NumbersRec(a + 1, b) + $"{a} "; //Заполняем строку справа.
+        else return string.Empty;
+}
+
+Console.WriteLine(NumbersFor(1,10)); //10 входит.
+Console.WriteLine(NumbersRec(1,10)); //10 входит.
+
+ //=====================
+
+ // 2. Сумма чисел от 1 до n.
+
+int SumFor(int n)
+{
+    int result = 0;
+    for(int i = 1; i <= n; i++) result += i;
+    return result;
+}
+
+int SumRec(int n)
+{
+    if(n == 0) return 0; //возвращаем нейтральный для сложения элемент.
+    else return n + SumRec(n - 1);
+}
+
+Console.WriteLine(SumFor(10)); //55
+Console.WriteLine(SumRec(10)); //55
